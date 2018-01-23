00b7f2b752cef239e42aa3d1d40edf1e6c688b5c
Yury Glushkov <mail@glushkov.us>
2018-01-24 00:57:16 +0300

    HttpCheck attributes added

--- src/Lykke.Service.BlockchainWallets.Core/Settings/BlockchainIntegrationSettings/BlockchainSettings.cs
+++ src/Lykke.Service.BlockchainWallets.Core/Settings/BlockchainIntegrationSettings/BlockchainSettings.cs
@@ -1,11 +1,15 @@
-﻿namespace Lykke.Service.BlockchainWallets.Core.Settings.BlockchainIntegrationSettings
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.Service.BlockchainWallets.Core.Settings.BlockchainIntegrationSettings
 {
     public class BlockchainSettings
     {
         public string Type { get; set; }
 
+        [HttpCheck("/api/isalive")]
         public string ApiUrl { get; set; }
 
+        [HttpCheck("/api/isalive")]
         public string SignFacadeUrl { get; set; }
 
         public string HotWalletAddress { get; set; }
