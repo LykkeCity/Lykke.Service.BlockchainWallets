4c9327915157240a50ef6fddfb8b8353359230ca
Yury Glushkov <mail@glushkov.us>
2018-03-27 16:51:15 +0300

    BlockchainSignFacade support added

---
+++ src/Lykke.Service.BlockchainWallets.Core/Settings/BlockchainSignFacadeClient/BlockchainSignFacadeClientSettings.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+
+namespace Lykke.Service.BlockchainWallets.Core.Settings.BlockchainSignFacadeClient
+{
+    [UsedImplicitly]
+    public class BlockchainSignFacadeClientSettings
+    {
+        [UsedImplicitly(ImplicitUseKindFlags.Assign)]
+        public string ServiceUrl { get; set; }
+    }
+}
--- src/Lykke.Service.BlockchainWallets.Core/Settings/ServiceSettings/BlockchainWalletsSettings.cs
+++ src/Lykke.Service.BlockchainWallets.Core/Settings/ServiceSettings/BlockchainWalletsSettings.cs
@@ -1,9 +1,17 @@
-﻿namespace Lykke.Service.BlockchainWallets.Core.Settings.ServiceSettings
+﻿using JetBrains.Annotations;
+
+namespace Lykke.Service.BlockchainWallets.Core.Settings.ServiceSettings
 {
+    [UsedImplicitly]
     public class BlockchainWalletsSettings
     {
+        [UsedImplicitly(ImplicitUseKindFlags.Assign)]
         public CqrsSettings Cqrs { get; set; }
 
+        [UsedImplicitly(ImplicitUseKindFlags.Assign)]
         public DbSettings Db { get; set; }
+
+        [UsedImplicitly(ImplicitUseKindFlags.Assign)]
+        public string SignFacadeApiKey { get; set; }
     }
 }
