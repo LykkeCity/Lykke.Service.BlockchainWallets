2b5c8ff0199f63d5f4fe972dff96fd38b3c8458b
Yury Glushkov <mail@glushkov.us>
2018-06-08 13:55:25 +0300

    LWDEV-5220 Some comments added

--- src/Lykke.Service.BlockchainWallets/Workflow/CommandHandlers/EndTransactionHistoryMonitoringCommandHandler.cs
+++ src/Lykke.Service.BlockchainWallets/Workflow/CommandHandlers/EndTransactionHistoryMonitoringCommandHandler.cs
@@ -52,10 +52,13 @@ namespace Lykke.Service.BlockchainWallets.Workflow.CommandHandlers
                 subscriptionType: subscriptionType
             );
             
+            // TODO: Fix potential issue with subscription/unsubscription race conditions
+            // If we have no subscriptions for address-asset pairs for specified address...
             if (await _monitoringSubscriptionRepository.WalletSubscriptionsCount(blockchainType, address, subscriptionType) == 0)
             {
                 try
                 {
+                    // Unsubscribe from address transactions observation (for all assets)
                     await apiClient.StopHistoryObservationOfIncomingTransactionsAsync(address);
                 }
                 catch (ErrorResponseException e) when (e.StatusCode == HttpStatusCode.NoContent)
