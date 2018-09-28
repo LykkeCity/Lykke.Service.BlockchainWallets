f3a0ce4a93697c13bfb5194cac765b45b04ac507
AleksandrBelousov <tortyt1@gmail.com>
2018-09-28 22:55:57 +0400

    Fixed tests for TC

--- tests/Lykke.Service.BlockchainWallets.CTests/Lykke.Service.BlockchainWallets.CTests/Utils/LaunchSettingsFixture.cs
+++ tests/Lykke.Service.BlockchainWallets.CTests/Lykke.Service.BlockchainWallets.CTests/Utils/LaunchSettingsFixture.cs
@@ -12,24 +12,31 @@ namespace Lykke.Service.BlockchainWallets.CTests.Utils
     {
         public LaunchSettingsFixture()
         {
-            using (var file = File.OpenText("Properties\\launchSettings.json"))
+            try
             {
-                var reader = new JsonTextReader(file);
-                var jObject = JObject.Load(reader);
-
-                var variables = jObject
-                    .GetValue("profiles")
-                    //select a proper profile here
-                    .SelectMany(profiles => profiles.Children())
-                    .SelectMany(profile => profile.Children<JProperty>())
-                    .Where(prop => prop.Name == "environmentVariables")
-                    .SelectMany(prop => prop.Value.Children<JProperty>())
-                    .ToList();
-
-                foreach (var variable in variables)
+                using (var file = File.OpenText("Properties\\launchSettings.json"))
                 {
-                    Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
+                    var reader = new JsonTextReader(file);
+                    var jObject = JObject.Load(reader);
+
+                    var variables = jObject
+                        .GetValue("profiles")
+                        //select a proper profile here
+                        .SelectMany(profiles => profiles.Children())
+                        .SelectMany(profile => profile.Children<JProperty>())
+                        .Where(prop => prop.Name == "environmentVariables")
+                        .SelectMany(prop => prop.Value.Children<JProperty>())
+                        .ToList();
+
+                    foreach (var variable in variables)
+                    {
+                        Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
+                    }
                 }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
         }
 
