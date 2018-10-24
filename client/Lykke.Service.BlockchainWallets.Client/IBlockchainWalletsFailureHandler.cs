740662a1d8121449af1d58a32301e8667c7eb63f
some-dev <some-dev@gmail.com>
2018-10-25 00:56:46 +0300

    add BlockchainWalletsFailureHandler interface

---
+++ client/Lykke.Service.BlockchainWallets.Client/IBlockchainWalletsFailureHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Lykke.Service.BlockchainWallets.Client
+{
+    public interface IBlockchainWalletsFailureHandler
+    {
+        Task<T> Execute<T>(Func<Task<T>> method, TimeSpan? timeout = null, Func<T> fallbackResult = null);
+    }
+}
