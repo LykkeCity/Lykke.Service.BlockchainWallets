abe244ab20519409aec46803c242a6c72f2f7aa7
some-dev <some-dev@gmail.com>
2019-03-12 17:58:07 +0300

    made prj build

---
+++ src/Lykke.Service.BlockchainWallets.AzureRepositories/Backup/BlockchainWalletBackupEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Lykke.Service.BlockchainWallets.Contract;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Lykke.Service.BlockchainWallets.AzureRepositories.Backup
+{
+    internal class BlockchainWalletBackupEntity:TableEntity
+    {
+        public static string GetPartitionKey(Guid clientId)
+        {
+            return clientId.ToString();
+        }
+
+        public static string GetRowKey(string address, string integrationLayerId)
+        {
+            return $"{integrationLayerId}_{address}";
+        }
+
+        public string Address { get; set; }
+
+        public string IntegrationLayerId { get; set; }
+
+        public Guid ClientId { get; set; }
+
+        public CreatorType CreatedBy { get; set; }
+    }
+}
