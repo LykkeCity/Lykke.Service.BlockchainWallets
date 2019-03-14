509d583d5388fe360920a8fe79975695d22b28af
some-dev <some-dev@gmail.com>
2019-03-14 04:07:50 +0300

    ref

--- src/Lykke.Service.BlockchainWallets.MongoRepositories/Wallets/BlockchainWalletMongoRepository.cs
+++ src/Lykke.Service.BlockchainWallets.MongoRepositories/Wallets/BlockchainWalletMongoRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Common.Log;
-using Lykke.AzureStorage;
 using Lykke.Common.Log;
 using Lykke.Service.BlockchainWallets.Contract;
 using Lykke.Service.BlockchainWallets.Core.DTOs;
